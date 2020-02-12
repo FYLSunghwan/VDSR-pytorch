fb862e97756078db2d5def095d46cc22a07cd014
FYLSunghwan <sunghwan519@hotmail.com>
2020-02-12 16:15:55 +0900

    add: Add bicubic upscaling in demo.py

--- demo.py
+++ demo.py
@@ -3,10 +3,19 @@ from torchvision.transforms import *
 from model import Net
 from torchvision.utils import save_image
 from PIL import Image
+import numpy as np
     
+def resize_img(img_np, height, width, resample=Image.BICUBIC):
+    img_np = np.asarray(img_np)
+    img = Image.fromarray(img_np)
+    resized = img.resize((width, height), resample=resample)
+    return np.array(resized)
+
+
 def load_img(filepath):
     img = Image.open(filepath)
     return img
+
 
 def main():
     
@@ -15,6 +24,9 @@ def main():
     
     model = torch.load('checkpoint/model_epoch_36.pth')['model'].to(device)
     img = load_img('test.jpg')
+    w, h = img.size[0], img.size[1]
+    img = resize_img(img, h*2, w*2)
+    print(h*2,w*2)
     transform = transforms.Compose([transforms.ToTensor()])
     model_in = transform(img).unsqueeze(0).to(device)
     model_out = model(model_in).cpu()
